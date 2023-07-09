790e24851fb9dbee327a15222cb6c6ce46096916
hectorpuga <hectorpuga@users.noreply.github.com>
2023-07-09 18:54:22 +0000

    deploy: 96f2755127c8127ee37b48bc247dfaed266f824c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "25143a163133927314da84c5cc4ad023",
-"/": "25143a163133927314da84c5cc4ad023",
+const RESOURCES = {"index.html": "e06c02b3c2bbeb5f8c03dccb5636d329",
+"/": "e06c02b3c2bbeb5f8c03dccb5636d329",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "e12aa1362913ab84fbcb42cda40f769e",
+"main.dart.js": "381829596379b872751d50c2ce233081",
 "version.json": "29cf846e56754da2a44a26c2e390070d",
 "canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
 "canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
